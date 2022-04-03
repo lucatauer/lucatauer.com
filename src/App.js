600ef996804aaa2ec666ea9b1221b319925db4b3
lucatauer <accounts@lucatauer.com>
2022-04-03 13:40:54 +0200

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './input.css';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
